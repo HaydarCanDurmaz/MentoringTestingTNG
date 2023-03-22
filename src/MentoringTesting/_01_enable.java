7cb9c684ff9ef42ce7aef33847de0dbb4af3786f
HaydarCanDurmaz <candrhydr@hotmail.com>
2023-03-22 10:33:16 +0300

    Initial commit

---
+++ src/MentoringTesting/_01_enable.java
@@ -0,0 +1,20 @@
+package MentoringTesting;
+
+import org.testng.annotations.Test;
+
+public class _01_enable {
+    /*org.seleniumhq.selenium:selenium-java:4.8.1
+org.testng:testng:6.14.3*/
+
+    @Test
+    public void c_language(){
+        System.out.println("C language");
+    }
+    @Test
+    public void jira(){
+        System.out.println("jira ile languaga");
+    }  @Test
+    public void java(){
+        System.out.println("java ile language");
+    }
+}
